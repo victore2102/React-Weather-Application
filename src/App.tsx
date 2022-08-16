e586c53377e8a9a702b59af35560f187960c5117
Victor Ekpenyong <vbe4@txstate.edu>
2022-08-16 18:25:13 -0500

    feat: blank slate, next commit will use branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import start from './start';
 import './App.css';
+import Start from './start';
+
+const api = {
+  key: "a2afcf79342a908e248e9b85ddc69e4f",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h3>Welcome To My Project</h3>
+        <h2>React Weather Application</h2>
       </header>
+      <Start />
+      <footer>
+        <h3>Created By:</h3>
+        <h2>Victor Ekpenyong</h2>
+      </footer>
     </div>
   );
 }
---
+++ src/start.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const api = {
+  key: "a2afcf79342a908e248e9b85ddc69e4f",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+export default function Start() {
+  return (
+    <button 
+    //onClick={}
+    >Click To Begin
+    </button>
+  );
+}
+
