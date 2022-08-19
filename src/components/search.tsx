ec976ce2dc6467803a47cad274b1c0272a7a6800
Victor Ekpenyong <vbe4@txstate.edu>
2022-08-19 17:03:04 -0500

    major changes to search page for city, working on API functionalities

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,26 +1,88 @@
 import { useState } from 'react';
+import '../index.css';
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
+
+
+const api = {
+  key: "a2afcf79342a908e248e9b85ddc69e4f",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 export default function Search()
 {
-    const [search, setSearch] = useState<string>("bruh");
-    const [searchData, setSearchData] = useState<string>("null");
 
-    function handleOnChange(searchData:string) {
-        setSearch(searchData);
-        //  onSearchChange(searchData);
+    const navigate = useNavigate();
+
+    function datebuilder(d: Date) {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`
     }
 
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const find = (evt: React.KeyboardEvent) => {
+        if(evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+            .then(res => res.json())
+            .then(result => {
+                setWeather(result)
+                setQuery('');
+                console.log(result);
+            });
+        }
+    }
+
+
+
     return (
-        <div className="App">
-            <h1>Look It Worked!</h1>
-            <div className='search-box'>
+        <div className="Search">
+            <header>
                 <input 
                     type='text'
-                    className='search-bar'
-                    placeholder='Search'
+                    className='Search-Input'
+                    placeholder='Search...'
+                    onChange={e => setQuery(e.target.value)}
+                    value={query}
+                    onKeyPress={find}
                 />
-            </div>
+            </header>
+            {(typeof weather.main != "undefined") ? (
+                <div>
+                    <div className='location-box'>
+                        <div className='location'>
+                            {weather.name}, {weather.sys.country}
+                        </div>
+                        <div className='date'>
+                            {datebuilder(new Date())}
+                        </div>
+                    </div>
+                    <div className="weather-box">
+                        <div className='temp'>
+                            80°F
+                        </div>
+                        <div className='weather'>
+                            Sunny
+                        </div>
+                    </div>
+                </div>
+            ) : ('')}
+            <footer className='footer'>
+                <button
+                className='Button'
+                onClick={() => navigate("/")}
+                >
+                    Start Page
+                </button>
+            </footer>
         </div>
     );
 }
--- src/start.tsx
+++ src/start.tsx
@@ -1,35 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, useNavigate } from 'react-router-dom';
-import start from './start';
+import './index.css';
 import './App.css';
-import Start from './start';
 
-const api = {
-  key: "a2afcf79342a908e248e9b85ddc69e4f",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
 
-function App() {
+export default function Start() {
 
     const navigate = useNavigate();
+
     return (
-        <div className="App">
+        <div className="Start">
         <header>
             <h3>Welcome To My Project</h3>
             <h2>React Weather Application</h2>
         </header>
         <button
+            className='Button'
             onClick={() => navigate("/search")}
         >
             Click To Begin
         </button>
-        <footer>
+        <footer className='footer'>
             <h3>Created By:</h3>
             <h2>Victor Ekpenyong</h2>
         </footer>
         </div>
     );
 }
-
-export default App;
